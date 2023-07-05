e8a83e09b6113a08e03b604fd19234b3299ba84a
Sebastienkusz <kusz.sebastien@gmail.com>
2023-07-05 09:12:00 +0200

    Config ssh add

--- ansible/script_ansible.sh
+++ ansible/script_ansible.sh
@@ -1,19 +1,3 @@
 #/bin/bash
 
 # Ping de bastion
-
-
-
-# #B1E3
-# Host bastion
-#         Hostname sebastienk-bastion.westeurope.cloudapp.azure.com
-#         User azureuser
-#         Port 22
-#         IdentityFile ~/.ssh/sebastien_rsa
-#         ForwardAgent yes
-
-# Host appli
-#         Hostname 10.1.0.4
-#         User azureuser
-#         Port 22
-#         ProxyJump bastion
