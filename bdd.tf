ebce2c6b4675e1e6b5cd529716e949d2b4f32d12
Sebastienkusz <kusz.sebastien@gmail.com>
2023-07-18 16:56:36 +0200

    monitor in progress

--- bdd.tf
+++ bdd.tf
@@ -68,13 +68,13 @@ resource "azurerm_network_security_rule" "mariadb_rule" {
 
 #Create private dns zone
 resource "azurerm_private_dns_zone" "dnszone" {
-  name                = "${local.mariadb_private_dns_zone}"
+  name                = local.mariadb_private_dns_zone
   resource_group_name = local.resource_group_name
 }
 
 #Create a link between private dns zone and virtual network
 resource "azurerm_private_dns_zone_virtual_network_link" "vnetlink" {
-  name                  = "${local.mariadb_private_dns_link}"
+  name                  = local.mariadb_private_dns_link
   resource_group_name   = local.resource_group_name
   private_dns_zone_name = azurerm_private_dns_zone.dnszone.name
   virtual_network_id    = azurerm_virtual_network.VNet.id
@@ -82,7 +82,7 @@ resource "azurerm_private_dns_zone_virtual_network_link" "vnetlink" {
 
 #Create endpoint
 resource "azurerm_private_endpoint" "pep" {
-  name                = "${local.mariadb_private_endpoint}"
+  name                = local.mariadb_private_endpoint
   location            = local.location
   resource_group_name = local.resource_group_name
   subnet_id           = azurerm_subnet.Subnet["sr2"].id
